1b34867955d842635d3322517ade9bdea40e81d2
AbuBakar <abubakarmujahid980@gmail.com>
2025-05-19 19:34:25 +0500

    2nd Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,18 @@ import { Route, Routes } from "react-router-dom"
 import Login from "./components/Login"
 import Signup from "./components/Signup"
 import HomePage from "./components/home/HomePage"
+import Footer from "./components/Footer/Footer"
 
 const App = () => {
   return (
+    <>
     <Routes>
       <Route path="/" element={<HomePage/>} />
       <Route path="/login" element={<Login/>} />
       <Route path="/signup" element={<Signup/>} />
     </Routes>
+    <Footer/>
+    </>
   )
 }
 
